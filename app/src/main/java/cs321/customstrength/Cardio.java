2d208d3a6b6eb292a764efa1b256a4a8147f0bf4
bbui1997 <bbui1997@gmail.com>
2016-11-23 14:09:35 -0500

    Update Cardio.java
    Made all constructors for each type of exercise use only 1 constructor
    The logic required for each constructor (ie. rep values in Strength) should belong in a different method
    The pseudo-code is explained in each class.
    
    Reasoning for removing fixedVolume and fixedIntensity from Cardio:
    If we want to allow the user to have a non-fixedVolume or non-fixedIntensity, will are required to make something like "int set"
    for Cardio, which in my mind doesn't really make sense. For example, if the person wants to jog for 9 minutes and sprint for the last minute,
    I think they are smart enough to do so instead of having two "sets" of running where they have one set that has a volume = 9 and intensity = moderate
    then a "set" of volume = 1 and intensity = very_high. I really don't think it's necessary.

--- app/src/main/java/cs321/customstrength/Cardio.java
+++ app/src/main/java/cs321/customstrength/Cardio.java
@@ -1,28 +1,31 @@
-ArrayList<String>ArrayList<String>package cs321.customstrength;
+package cs321.customstrength;
 
 import java.util.ArrayList;
 
 class Cardio extends Exercise {
   int time; //in minutes
   Intensity intensity;
-  //fixedVolume==true && fixedIntensity==true
-  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String>ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity, int time, Intensity intensity) {
-    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
+  
+  Cardio(String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, 
+         String mechanics, String level, String force,
+         int time, Intensity intensity) {
+    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, mechanics, level, force);
     this.time=time;
     this.intensity=intensity;
   }
-  //fixedVolume==true && fixedIntensity==false
-  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity, int time) {
-    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
-    this.time=time;
-  }
-  //fixedVolume==false && fixedIntensity==true
-  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity, Intensity intensity) {
-    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
-    this.intensity=intensity;
-  }
-  //fixedVolume==false && fixedIntensity==false
-  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity) {
-    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
-  }
+   
+//  //fixedVolume==true && fixedIntensity==false
+//  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity, int time) {
+//    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
+//    this.time=time;
+//  }
+//  //fixedVolume==false && fixedIntensity==true
+//  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity, Intensity intensity) {
+//    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
+//    this.intensity=intensity;
+//  }
+//  //fixedVolume==false && fixedIntensity==false
+//  Cardio (String name, String primaryMuscle, ArrayList<String> secondaryMuscles, ArrayList<String> equipmentUsed, String level, boolean fixedVolume, boolean fixedIntensity) {
+//    super(name, primaryMuscle, secondaryMuscles, equipmentUsed, level, fixedVolume, fixedIntensity);
+//  }
 }
