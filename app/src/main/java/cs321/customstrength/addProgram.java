05b6974d6d5165a65310d86b1b711ddce0ce4c45
nickvalin1 <nvalin@masonlive.gmu.edu>
2016-11-26 01:04:47 -0500

    Added some actual data storage

--- app/src/main/java/cs321/customstrength/addProgram.java
+++ app/src/main/java/cs321/customstrength/addProgram.java
@@ -1,18 +1,13 @@
 package cs321.customstrength;
 
 import android.content.Intent;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.support.v7.widget.SearchView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
 
 public class addProgram extends AppCompatActivity {
@@ -20,6 +15,9 @@ public class addProgram extends AppCompatActivity {
     LinearLayout weeks;
     TextView dayValue;
     LinearLayout mainLayout;
+    ArrayList<Week> weeksArray=new ArrayList<Week>();
+    ExerciseData exercise;
+    Day day;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,27 +64,18 @@ public class addProgram extends AppCompatActivity {
     public void createWeek() {
         LinearLayout currentWeek = new LinearLayout(this);
         currentWeek.setOrientation(LinearLayout.VERTICAL);
-        TextView title = new TextView(this);
-        title.setText("\nWeek:");
-
-        currentWeek.addView(title);
-
-        LinearLayout name = new LinearLayout(this);
-
-        TextView nameText = new TextView(this);
-        nameText.setText("Name:");
-        name.addView(nameText);
 
         EditText nameInput = new EditText(this);
         nameInput.setInputType(1);
-        name.addView(nameInput);
+        nameInput.setText("Name of Week");
 
-        currentWeek.addView(name);
+        currentWeek.addView(nameInput);
 
         LinearLayout days = new LinearLayout(this);
         days.setOrientation(LinearLayout.VERTICAL);
 
-        addDay(days);
+        Week week=new Week();
+        week.days=addDay(days);
 
         currentWeek.addView(days);
 
@@ -97,27 +86,47 @@ public class addProgram extends AppCompatActivity {
         weeks.removeView(weeks.getChildAt(weeks.getChildCount() - 1));
     }
 
-    private void addDay(LinearLayout days) {
+    private ArrayList<Day> addDay(LinearLayout days) {
+        ArrayList<Day> daysArray=new ArrayList<Day>();
+        //for number of days in the week
         for (int i = 0; i < Character.getNumericValue(dayValue.getText().charAt(0)); i++) {
+            //Linear layout for the day
             LinearLayout currentDay = new LinearLayout(this);
             currentDay.setOrientation(LinearLayout.VERTICAL);
 
+            //Name of the day
             EditText nameInput = new EditText(this);
             nameInput.setInputType(1);
-            nameInput.setText("Name of Day " + i);
+            nameInput.setText("Name of Day " + (i+1));
             currentDay.addView(nameInput);
 
+            //Add exercises to the day
+            day=new Day("Day "+(i+1));
             Button addExercise = new Button(this);
             addExercise.setText("Add Exercise");
             addExercise.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
+                    //empty out exercise
+                    exercise=null;
                     Intent searchIntent=new Intent(view.getContext(), selectExercise.class);
-                    startActivity(searchIntent);
+                    startActivityForResult(searchIntent, 1);
+                    day.exercises.add(exercise);
                 }
             });
             currentDay.addView(addExercise);
 
             days.addView(currentDay);
+
+            daysArray.add(day);
+        }
+        return daysArray;
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==1 && requestCode==RESULT_OK && data!=null) {
+            String toAdd=data.getStringExtra("Exercise");
+            //make this into an exercise not exerciseData
+            //also get sets, reps, and weight from this page
+            exercise=LoadExerciseData.PRELOADED_EXERCISES.get(toAdd);
         }
     }
 }
